b00adb04b3cd4e2980d6bcf75ae0cc35702a04f6
password123456 <sendmefuture@gmail.com>
2024-11-26 10:42:52 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ class Config:
     }
 
     SLACK_BOT = 'your-slack-bot-token'
+    CHANNEL_ALL_USERS_IN = 'The channel ID that all users are joined to in order to collect information about Slack users.'
 
     APP_PATH = os.path.dirname(os.path.abspath(__file__))
     KEY_DB = os.path.join(APP_PATH, 'app/db/keys.db')
