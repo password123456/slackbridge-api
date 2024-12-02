9fa6a3c8c771d961bb0c7b4f5a476d54397dafaa
password123456 <sendmefuture@gmail.com>
2024-12-03 08:26:09 +0900

    Update search.py

--- app/search.py
+++ app/search.py
@@ -1,6 +1,6 @@
 __author__ = 'https://github.com/password123456/'
-__date__ = '2024.11.19'
-__version__ = '1.1'
+__date__ = '2024.12.03'
+__version__ = '1.2'
 __status__ = 'Production'
 
 from flask import current_app
@@ -47,7 +47,36 @@ def search_email(email):
                         'user_email': user_email
                     }
                     member_id = member_info['member_id']
-                    break  # if found, stop search
+                    break  # if found, stop searching
         return member_id, member_info
     except FileNotFoundError:
+        return None
+
+
+def search_member_ids(email_list):
+    member_ids = []
+
+    if isinstance(email_list, str):
+        email_list = [email_list]
+
+    try:
+        with open(current_app.config['USERS_DB'], 'r', encoding='utf-8') as database:
+            for line in database:
+                if line.startswith('#') or len(line.strip()) == 0:
+                    continue
+                split_line = line.split(',')
+                user_email = split_line[5].strip()
+                for email in email_list:
+                    if email.strip() == user_email:
+                        member_id = split_line[4].strip()
+                        member_ids.append(member_id)
+    except FileNotFoundError:
+        return None
+
+    if member_ids:
+        if len(member_ids) == 1:
+            return member_ids[0]
+        elif member_ids:
+            return ','.join(member_ids)
+    else:
         return None
